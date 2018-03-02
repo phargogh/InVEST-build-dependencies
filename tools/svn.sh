d0d37d37cc38cf42fbbc4bc5196075e58ba7fceb
James Douglass <jdouglass@stanford.edu>
2018-03-02 15:12:52 -0800

    Adding vcforpython27 and svn.

---
+++ tools/svn.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+wget https://svwh.dl.sourceforge.net/project/win32svn/1.8.17/Setup-Subversion-1.8.17.msi
+
+
+
---
+++ tools/vcforpython27.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+wget https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi
+
+
+
