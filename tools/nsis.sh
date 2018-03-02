110913932a856547ca449cefd0dca823a6e652ce
James Douglass <jdouglass@stanford.edu>
2018-03-02 14:30:03 -0800

    Adding an NSIS script that includes plugins.
    Currently gets NSIS stuff from dropbox because sourceforge has been down
    a couple days.

---
+++ tools/nsis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash -e
+
+DROPBOX="$HOME/Dropbox/installers-for-natcap-build-processes"
+DESTDIR=NSIS-2.51
+
+if [ -d $DESTDIR ]
+then
+    rm -r $DESTDIR
+fi
+
+mkdir -p $DESTDIR
+
+cp $DROPBOX/NSIS/* .
+
+7z x -o$DESTDIR nsis-2.51-setup.exe
+
+for zipfile in `Inetc.zip Nsisunz.zip`
+do
+    unzip -d $DESTDIR $zipfile
+done;
+
+7z x -o$DESTDIR NsProcess.zip
+
+zip -r $DESTDIR.zip $DESTDIR
