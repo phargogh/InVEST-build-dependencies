14ba1332f3e2525599a97bfdc33d44c4923ab3e2
James Douglass <jdouglass@stanford.edu>
2018-03-06 12:51:29 -0800

    Changing git install from portablegit to git.

--- getall.sh
+++ getall.sh
@@ -23,7 +23,7 @@ alias wget="wget -P $TOOLSDIR"
 
 wget http://www.7-zip.org/a/7z1801-x64.msi
 wget https://svwh.dl.sourceforge.net/project/win32svn/1.8.17/Setup-Subversion-1.8.17.msi
-wget https://github.com/git-for-windows/git/releases/download/v2.16.2.windows.1/PortableGit-2.16.2-64-bit.7z.exe
+wget https://github.com/git-for-windows/git/releases/download/v2.16.2.windows.1/Git-2.16.2-64-bit.exe
 wget https://www.mercurial-scm.org/release/windows/Mercurial-4.5-x64.exe
 wget https://repo.continuum.io/miniconda/Miniconda2-4.4.10-Windows-x86.exe
 wget https://repo.continuum.io/miniconda/Miniconda2-4.4.10-Windows-x86_64.exe
