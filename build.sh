3ca0b86cb0e3f67fe8863acd7b183f2fe28f8661
James Douglass <jdouglass@stanford.edu>
2018-03-06 14:07:45 -0800

    Adding loops to check serial port output for confirmation setup is done.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 rev=`hg log -r. --template="r{rev}-c{date(date, '%Y%m%d')}-{node|short}"`
 templatename="jenkins-win2008r2-$rev"
 tempmachinename=$templatename-setupinprogress
+zone=us-west1-a
 
 #gcloud compute instance-templates create $templatename \
 #    --boot-disk-auto-delete \
@@ -12,7 +13,7 @@ tempmachinename=$templatename-setupinprogress
 #    --boot-disk-type=pd-standard \
 #    --machine-type=n1-standard-2 \
 #    --metadata-from-file sysprep-specialize-script-ps1=install_windows_build_dependencies.ps1 \
-#    --region=us-west1-a \
+#    --region=$zone \
 #    --image-project=windows-cloud \
 #    --image=windows-server-2008-r2-dc-v20180213 \
 #    --service-account=jenkins-build-cluster@natcap-servers.iam.gserviceaccount.com
@@ -24,14 +25,44 @@ gcloud compute instances create $tempmachinename \
     --boot-disk-type=pd-standard \
     --machine-type=n1-standard-2 \
     --metadata-from-file sysprep-specialize-script-ps1=install_windows_build_dependencies.ps1 \
-    --zone=us-west1-a \
+    --zone=$zone \
     --image-project=windows-cloud \
     --image=windows-server-2008-r2-dc-v20180213 \
     --service-account=jenkins-build-cluster@natcap-servers.iam.gserviceaccount.com
 
 
-# shut down the computer once setup finishes
+while true
+do
+    gcloud compute --project=natcap-servers \
+        instances get-serial-port-output \
+        $tempmachinename --zone=us-west1-a | tail -n 20 | grep "Activation successful"
+    if [ $? -eq 0 ]
+    then
+        # computer setup is complete, wait until the machine is completely shut down
+        # before creating the new machine image.
+        while true
+        do
+            gcloud compute --project=natcap-servers \
+                instances describe $tempmachinename | grep status | grep TERMINATED
+            if [ $? -eq 0 ]
+            then
+                gcloud compute images create $templatename \
+                    --source-disk=$tempmachinename \
+                    --source-disk-zone=$zone \
+                    --family=windows-2008-r2
 
-# create an image of the disk created by the new instance command.
+                # now, delete the temporary VM
+                gcloud compute instances delete $tempmachinename --zone=$zone
+                break
+            else
+                sleep 2
+            fi
+        done
+        break
+    else
+        # computer setup is still in progress 
+        sleep 2
+    fi
+done
 
 
