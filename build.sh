08f6bda0e1217cf912f9b1f2c8b4f4d382dbe686
James Douglass <jdouglass@stanford.edu>
2018-03-06 11:20:44 -0800

    Using windows-specific metadata, specifying image project.

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ gcloud compute instance-templates create $templatename \
     --boot-disk-size=50GB \
     --boot-disk-type=pd-standard \
     --machine-type=n1-standard-2 \
-    --metadata-from-file startup-script=install_windows_build_dependencies.ps1 \
+    --metadata-from-file sysprep-specialize-script-ps1=install_windows_build_dependencies.ps1 \
     --region=us-west1-a \
+    --image-project=windows-cloud \
     --image=windows-server-2008-r2-dc-v20180213 \
     --service-account=jenkins-build-cluster@natcap-servers.iam.gserviceaccount.com
 
