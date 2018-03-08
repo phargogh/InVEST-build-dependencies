9de729f8e7d930908081c15b73f784d60801e94f
James Douglass <jdouglass@stanford.edu>
2018-03-08 13:31:25 -0800

    Using whatever version of qtpy comes along.

--- install_windows_build_dependencies.ps1
+++ install_windows_build_dependencies.ps1
@@ -48,7 +48,7 @@ function InstallNSISPluginFromZipfile
 function InstallAnticipatedCondaPackages
 {
     param([string]$condainstall)
-    cmd.exe /C "$condainstall\Scripts\conda" install -y python=2.7 "gdal>=2.0" matplotlib rtree shapely "qtpy<1.3"
+    cmd.exe /C "$condainstall\Scripts\conda" install -y python=2.7 "gdal>=2" matplotlib rtree shapely qtpy
     cmd.exe /C "$condainstall\Scripts\conda" update -n base conda
 }
 
